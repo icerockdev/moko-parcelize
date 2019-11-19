e2da7cdd94a4946c23d8bd5a8a427c66fd114454
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-19 10:29:34 +0700

    update to kotlin 1.3.60

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
         const val minSdk = 16
     }
 
-    const val kotlin = "1.3.50"
+    const val kotlin = "1.3.60"
 
     object Plugins {
         const val kotlin = Versions.kotlin
