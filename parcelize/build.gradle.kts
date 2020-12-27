71f39d63cb7fab09adfab1dcae1dd33667e1f266
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-12-27 20:57:30 +0700

    kotlin 1.4.30

--- parcelize/build.gradle.kts
+++ parcelize/build.gradle.kts
@@ -5,7 +5,7 @@
 plugins {
     plugin(Deps.Plugins.androidLibrary)
     plugin(Deps.Plugins.kotlinMultiPlatform)
-    plugin(Deps.Plugins.androidExtensions)
+    plugin(Deps.Plugins.kotlinParcelize)
     plugin(Deps.Plugins.mobileMultiPlatform)
     plugin(Deps.Plugins.mavenPublish)
 }
