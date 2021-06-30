33f6b5c5424869f5520c020be7712cb7b9ef594d
Anton Shestak <ashestak@icerockdev.com>
2021-06-30 12:39:52 +0700

    #29 update kotlin and create @OptionalExpectation stub

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     }
     dependencies {
         classpath("dev.icerock:mobile-multiplatform:0.9.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
         classpath("com.android.tools.build:gradle:4.2.1")
     }
 }
